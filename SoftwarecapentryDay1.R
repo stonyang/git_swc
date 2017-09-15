44983317de619387c4f17df6c599ad5835a77e2c
Stanley Omondi Onyango <StanleyOmondi.Onyango@UGent.be>
2017-09-15 10:25:33 +0200

    Start notes on Mars as a base

---
+++ SoftwarecapentryDay1.R
@@ -0,0 +1,40 @@
+1 + 300
+1+
+  3 + 5 * 2
+2e5
+2/10000
+#
+log10(23)
+?log
+1 == 1
+?all
+x <- 1/40
+log(x)
+x <- 100
+y <- x+1
+sta.akisa <- 70
+1:5
+x <- 2^(1:5)
+2^(1:5)
+x <- 2^(1:5)
+ls()
+ls(all.names = TRUE)
+rm(x)
+current.environment <- ls()
+rm(list <- ls() )
+rm(list)
+rm(ls)
+sessionInfo()
+min_height
+x <- min_height
+x <- max.heigt
+x <- _age
+max.height()
+vignette("ggplot2-specs")
+??log
+c(1, 2, 3)
+c('d', 'e', 'f')
+c(1, 2, 'f')
+combovect <- c(1, 2, 'f')
+class (combovect)
+getwd()
